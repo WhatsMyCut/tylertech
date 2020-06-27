f9f4891272e266685f0dfc68011b08ada5d9eb1b
Mike Taylor <mike@whatsmycut.com>
2020-06-27 01:37:59 -0700

    first commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+	export let segment;
+</script>
+
+<svelte:head>
+<style lang=”scss” global>
+  @import "./style/global.scss";
+</style>
+</svelte:head>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,45 @@
+<style lang="scss">
+  @import "../style/theme.scss";
+
+	h1, figure {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 600px;
+		margin: 0 0 1em 0;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Tyler Tech Booth</title>
+  <style lang=”scss” global>
+    @import "./style/global.scss";
+  </style>
+</svelte:head>
+
+<h1>Tyler Tech Booth</h1>
+
+<figure>
+	<figcaption>Have fun with Sapper!</figcaption>
+	<img alt='Tyler Tech Booth' src='Booth_Template_Guide.png'>
+</figure>
