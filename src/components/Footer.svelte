b952d9f696424eed8ff136f7c3e2fdb67476d6ff
Mike Taylor <mike@whatsmycut.com>
2020-07-02 23:39:31 -0700

    Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,9 @@
+<script>
+  let date = new Date().getFullYear();
+</script>
+<style type="text/sass">
+  @import './Footer.scss';
+</style>
+<footer class="footer">
+  <div>{ date } &copy; <a href="http://www.tylertech.com" alt="Tyler Technologies" target="_tt">Tyler Technologies</a></div>
+</footer>
