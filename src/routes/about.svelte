39a9921e197366cd5e986bac2c90f859a2b6c7cb
Mike Taylor <mike@whatsmycut.com>
2020-07-02 23:41:08 -0700

    quick mockup for the client for video permission error

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,13 +4,9 @@
 
 <style type="text/sass">
   @import '../style/global.scss';
-  h1 {
-    font-family: "Amplitude Bold";
-    color: #21578A;
-  }
 </style>
 
-<h1>Video</h1>
+<h1 class="headline">Video</h1>
 
 <div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><iframe src="https://fast.wistia.net/embed/iframe/vflet52iog?videoFoam=true" title="Land &amp; Official Records Virtual Trade Show Video" allow="autoplay; fullscreen" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen msallowfullscreen width="100%" height="100%"></iframe></div></div>
 <script src="https://fast.wistia.net/assets/external/E-v1.js" async></script>
