e92819d6cc0d23352f5d115fd46d268963a19257
Mike Taylor <mike@whatsmycut.com>
2020-07-02 23:40:13 -0700

    Remove links from nav (all routes)

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,5 @@
 <script>
-  export let segment;
+  // export let segment;
 </script>
 
 <style type="text/sass">
@@ -7,13 +7,7 @@
 </style>
 
 <nav>
-  <ul class="tester">
-    <li><img class="logo" src="/tylertech_logo.png" alt="Tyler Technologies" /></li>
-    <li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-    <li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">video</a></li>
-
-    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-         the blog data when we hover over the link or tap it on a touchscreen -->
-    <li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
+  <ul class="nav">
+    <li><a href="."><div class="logo" alt="Tyler Technologies" /></a></li>
   </ul>
 </nav>
