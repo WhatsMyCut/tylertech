08c9c007123b81d5ca7611c3cfab495179899294
Mike Taylor <mike@whatsmycut.com>
2020-07-13 03:38:41 -0700

    Warning message for smaller browsers

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -10,4 +10,5 @@
   <ul class="nav">
     <li><a href="."><div class="logo" alt="Tyler Technologies" /></a></li>
   </ul>
+  <div class="message">Thanks for visiting the Tyler Tech Experience. This interactive is best viewed on a modern desktop browser. Please visit us on your desktop for the best viewing experience.</div>
 </nav>
