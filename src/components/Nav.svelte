0c72beba3a4649868bf8e5c56f2d9e30cba6d629
Mike Taylor <mike@whatsmycut.com>
2020-07-13 14:44:39 -0700

    Text and color fixes

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -10,5 +10,5 @@
   <ul class="nav">
     <li><a href="."><div class="logo" alt="Tyler Technologies" /></a></li>
   </ul>
-  <div class="message">Thanks for visiting the Tyler Tech Experience. This interactive is best viewed on a modern desktop browser. Please visit us on your desktop for the best viewing experience.</div>
+  <div class="message">Thanks for visiting the Tyler Tech Experience. Please visit us on a modern desktop browser for the best viewing experience.</div>
 </nav>
