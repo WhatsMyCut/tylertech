e0ea9ebf506ef7a3d546f3bf9ea402eccd8b5c59
Mike Taylor <mike@whatsmycut.com>
2020-07-14 20:44:48 -0700

    https://heliosinteract.atlassian.net/browse/TT2-51

--- src/routes/_sections.js
+++ src/routes/_sections.js
@@ -42,7 +42,7 @@ const sections = [{
         type: "Video",
         text: "Watch Video",
         icon: "/icons/Video.svg",
-        content: '<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><iframe src="https://fast.wistia.net/embed/iframe/vflet52iog?videoFoam=true" title="Land &amp; Official Records Virtual Trade Show Video" allow="autoplay; fullscreen" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen msallowfullscreen width="100%" height="100%"></iframe></div></div><script src="https://fast.wistia.net/assets/external/E-v1.js" async></script>',
+        content: '<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><iframe src="https://fast.wistia.net/embed/iframe/vflet52iog?videoFoam=true" title="Land &amp; Official Records Virtual Trade Show Video" allow="autoplay; fullscreen; muted;" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen msallowfullscreen width="100%" height="100%"></iframe></div></div><script src="https://fast.wistia.net/assets/external/E-v1.js" async></script>',
       },
       "hdisplay": {
         type: "horiz-display",
@@ -173,7 +173,7 @@ const sections = [{
       },
       "pedestal3": {
         type: "PDF",
-        URL: "https://www.tylertech.com/erp-2",
+        URL: "https://www.tylertech.com/erp-3",
         text: "Read Success Story",
         icon: "/icons/SinglePageDoc.svg",
         content: '<img src="/icons/SinglePageDoc.svg" alt="Brochure" height="80" />',
